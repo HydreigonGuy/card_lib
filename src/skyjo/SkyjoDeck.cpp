db58e58baa2addccd49807f79581a38e91d21320
adrien <ad.thibault18@gmail.com>
2026-01-03 22:18:46 +0100

    updated deck to use correct number of each card

--- src/skyjo/SkyjoDeck.cpp
+++ src/skyjo/SkyjoDeck.cpp
@@ -3,7 +3,13 @@
 
 SkyjoDeck::SkyjoDeck() : Deck()
 {
-    for (int i = -2; i < 13; i++)
+    for (int j = 0; j < 5; j++)
+        this->addCard(std::make_shared<Card>(-2));
+    for (int j = 0; j < 10; j++)
+        this->addCard(std::make_shared<Card>(-1));
+    for (int j = 0; j < 15; j++)
+        this->addCard(std::make_shared<Card>(0));
+    for (int i = 1; i < 13; i++)
         for (int j = 0; j < 10; j++)
             this->addCard(std::make_shared<Card>(i));
 }
